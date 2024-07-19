1e36371ef1ed328c081c2e5627def4879bac8293
tarek hariri <linebreaker@users.noreply.github.com>
2024-07-19 18:51:50 -0400

    dd

--- public/scripts/acherre.js
+++ public/scripts/acherre.js
@@ -1,3 +1,26 @@
+var setMainHeight = function(){
+	var header = document.getElementById('headerMain');
+	var footer = document.getElementById('footerMain');
+	var main = document.getElementById('mainMain');
+	var availHeight = window.innerHeight;
+	var _availHeight = window.screen.availHeight;
+	console.log("window.screen.availHeight:", _availHeight);
+	var headerHeight = header.offsetHeight;
+	var footerHeight = footer.offsetHeight;
+	var mainHeight = main.offsetHeight;
+	var usedHeight = headerHeight + footerHeight + mainHeight;
+	var freeHeight = (availHeight - (headerHeight + footerHeight)) + 'px';
+	if (usedHeight < availHeight) {
+		main.style.height = freeHeight;
+		console.log("Free height is: " + freeHeight + "px while availHeight is: " + availHeight + "px.");
+	}
+}
+
+window.addEventListener("load", setMainHeight);
+window.addEventListener("resize", setMainHeight);
+window.addEventListener("click", setMainHeight);
+document.addEventListener("mouseover", setMainHeight);
+
 function _testFunction(e) {
 	console.log("Clicked:", e.innerText + " " + e.tagName);
 }
